9b84b7982dde691bef348fe12fe47891f03cfb39
alek1984 <154059778+alek1984@users.noreply.github.com>
2025-03-18 20:42:47 +0200

    Add files via upload

---
+++ pyproject.toml.py
@@ -0,0 +1,15 @@
+[tool.poetry]
+name = "personal-assistant"
+version = "0.1.0"
+description = "Personal Assistant CLI app"
+authors = ["Ваше ім'я <email@example.com>"]
+readme = "README.md"
+
+[tool.poetry.dependencies]
+python = "^3.10"
+pickleshare = "*"
+regex = "*"
+
+[build-system]
+requires = ["poetry-core"]
+build-backend = "poetry.core.masonry.api"
